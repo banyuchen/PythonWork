286f4740fbf99af347a0a099238d9ba893e7662e
wenzheng <1204470409@qq.com>
2019-04-27 09:45:10 +0800

    111

--- 使用API/自动化打包3.py
+++
@@ -1,2 +0,0 @@
-html = '<html>\n    ' + "<body>\n   " + "<h1>Hello</h1>\n   " + '<img src="' + "url" + '">\n    '+"<p>下载地址 " + "url" + "</p>\n" + "<p>蒲公英的更新会有延迟,具体版本时间以邮件时间为准</p>\n" + "</body>\n" + "</html>"
-print(html)
